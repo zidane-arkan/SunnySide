fc3ab4604bbd8f2457f3ff919ad1d0cc605fffbd
zidane-arkan <zidark897@Gmail.com>
2021-11-07 12:19:09 +0700

    Update 2.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Function
+function scroll(){
+    contentBox.scrollIntoView();
+}
+//Get Arrow Element
+let titleBox = document.getElementsByClassName("title-box")[0];
+let arrowDown = titleBox.querySelector("#arrow-icon").getElementsByClassName("arrow-down")[0];
+//Get Content Box Element
+let contentBox = document.getElementsByClassName("content")[0].querySelector("#content-box");
+//Function Arrow
+arrowDown.addEventListener('click',scroll );
