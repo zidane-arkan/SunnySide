25a981ac7ad6efed2cdf447403b5edc05decb9c2
zidane-arkan <zidark897@Gmail.com>
2021-11-20 13:01:40 +0700

    Update 4.0

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 function scrollDown(){
     contentBox.scrollIntoView();
 }
+
 //Get Arrow Element
 let titleBox = document.getElementsByClassName("title-box")[0];
 let arrowDown = titleBox.querySelector("#arrow-icon").getElementsByClassName("arrow-down")[0];
@@ -9,3 +10,6 @@ let arrowDown = titleBox.querySelector("#arrow-icon").getElementsByClassName("ar
 let contentBox = document.getElementsByClassName("content")[0].querySelector("#content-box");
 //Function Arrow
 arrowDown.addEventListener('click',scrollDown );
+
+
+//Change Navbar
